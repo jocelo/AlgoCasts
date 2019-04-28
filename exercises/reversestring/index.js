12638409abb3be73cf6d047e4f4e2fa9ecd79d56
Alfredo Alonso <jocelo@Alfredos-MBP.fios-router.home>
2019-04-28 00:09:44 -0400

    Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse_first(str) {
+  return str.split('').reverse().join('');
+}
+
+function reverse_second(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character)=>{
+    debugger;
+    return character + reversed;
+  }, '');
+}
 
 module.exports = reverse;
