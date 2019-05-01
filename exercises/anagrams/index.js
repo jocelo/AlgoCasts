c28e3f23218687d3ab0fa0bb313fd24a75c73f35
Alfredo Alonso <jocelo@Alfredos-MBP.fios-router.home>
2019-04-30 22:59:14 -0400

    Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams_first_attempt(stringA, stringB) {
+  function charMap(str) {
+    const charMap = {};
+    const cleanStr = str.replace(/[^\w]/g, '').toLowerCase();
+    for (let char of cleanStr) {
+      charMap[char] = charMap[char]+1 || 1;
+    }
+    return charMap;
+  }
+
+  const charMapA = charMap(stringA);
+  const charMapB = charMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function anagrams(stringA, stringB) {
+  function cleanString(str) {
+    return str.replace(/[^\w]/g, '')
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('');
+  }
+
+  return cleanString(stringA) === cleanString(stringB);
+}
 
 module.exports = anagrams;
