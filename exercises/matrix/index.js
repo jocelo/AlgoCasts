23b48fa017a35c236c3443eec9bb7a8d2c83923b
Alfredo Alonso <jocelo@Alfredos-MBP.fios-router.home>
2019-05-21 07:04:43 -0400

    Spiral matrix v1

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,55 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+	const resultSet = [];
+	for (let column=0 ; column<n ; column++) {
+		resultSet.push([]);
+	}
+
+	let counter = 1,
+		startCol = 0,
+		endCol = n-1,
+		startRow = 0,
+		endRow = n-1;
+
+	while (startCol <= endCol && startRow <= endRow) {
+		
+		// [ > ] left to right, same row
+		for (let i=startCol ; i<=endCol ; i++) {
+			resultSet[startRow][i] = counter;
+			counter++;
+		}
+		startRow++;
+
+		// [ v ] top to bottom, same column
+		for (let i=startRow ; i<=endRow ; i++) {
+			resultSet[i][endCol] = counter;
+			counter++;
+		}
+		endCol--;
+
+		// [ < ] right to left, same row
+		for (let i=endCol ; i>=startCol ; i--) {
+			resultSet[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+
+		// [ ^ ] bottom to top, same column
+		for (let i=endRow ; i>=startRow ; i--) {
+			resultSet[i][startCol] = counter;
+			counter++;
+		}
+		startCol++;
+
+	}
+
+	for (let i=0 ; i<n ; i++) {
+		console.log(resultSet[i])
+	}
+}
+
+matrix(5)
 
 module.exports = matrix;
