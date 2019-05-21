aff066fb3351d9d578cf8b5253cf077ec522b2c5
alfredo.alonso <alfredo.alonso@prodapt.com>
2019-05-21 10:06:28 -0400

    WIP spiral matrix v2

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,7 +15,106 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
+function getNextCoord(resultMatrix, row, col, size) {
+	// 0-Validate if next cell is valid
+	// 1-get direciont change?
+	// 2-get next cell
+	// 3-set value of cell
+	// 4-iterate 
+	console.log('----------------------');
+	console.log(col, col+1, row);
+	console.log('size', size)
+	console.log(resultMatrix)
+	console.log(resultMatrix[row][(col+1)], resultMatrix[row][(col+1)] === '0')
+	if (col+1 < size && resultMatrix[row][(col+1)] === '0') {
+		return {col:col+1, row:row}
+	}
+	
+	return false;
+}
+
 function matrix(n) {
+	const rowSet = [],
+		resultSet = [];
+	
+	let row = 0,
+		col = -1,
+		counter = 1,
+		coord = {};
+
+	for (let i=0 ; i<n ; i++) {
+		rowSet.push('0');
+	}
+	for (let i=0 ; i<n ; i++) {
+		resultSet.push(rowSet.slice());
+	}
+
+	while (counter < (n*n)) {
+		coord = getNextCoord(resultSet, row, col, n);
+		row = coord.row;
+		col = coord.col;
+
+		console.log('the next available coord is:', coord);
+		if (!coord) {
+			// short curcuit
+			counter = n*n;
+		} else {
+			resultSet[row][col] = counter;
+			counter++;
+		}
+	}
+/*
+	let direction = 'E';
+		col=0,
+		row=0,
+		keepGoing = true,
+		counter = 1;
+
+	while (keepGoing) {
+		console.log('['+row+']['+col+'] = ', counter);
+		
+		resultSet[row][col] = counter;
+
+		switch(direction) {
+			case 'N':
+				break;
+			case 'S':
+				if (
+						(row+1) <= n
+				) {
+					row++;
+				} else {
+					direction = 'W';
+				}
+				break;
+			case 'E':
+				if (
+						(col+1) <= n
+				) {
+					col++;
+				} else {
+					direction='S';
+					row++;
+				}
+				break;
+			case 'W':
+				break;
+			default:
+				return false;
+		}
+		
+		counter++;
+		if (counter === 20) {
+			keepGoing = false;
+		}
+	}
+*/
+	for (let i=0 ; i<n ; i++) {
+		console.log(resultSet[i]);
+	}
+}
+
+function matrix_iterative(n) {
 	const resultSet = [];
 	for (let column=0 ; column<n ; column++) {
 		resultSet.push([]);
@@ -61,5 +160,7 @@ function matrix(n) {
 
 	return resultSet;
 }
+
+matrix(3);
 
 module.exports = matrix;
