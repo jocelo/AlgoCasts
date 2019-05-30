aa49f47fc0430aeec610fc398f792bd8deb53f0a
jocelo <gopher@gmail.com>
2019-05-30 06:48:28 -0400

    Linked list: for of generator

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -141,6 +141,15 @@ class LinkedList {
 			node = node.next;
 		}
 	}
+
+	*[Symbol.iterator]() {
+		let node = this.head;
+
+		while(node) {
+			yield node;
+			node = node.next;
+		}
+	}
 }
 
 module.exports = { Node, LinkedList };
