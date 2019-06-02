9b1f70869801e99329ddfd590c9e127357b40b21
jocelo <gopher@gmail.com>
2019-06-02 14:05:25 -0400

    bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,19 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+	let tmp = 0;
 
+	for (let i=0 ; i<arr.length ; i++) {
+		for (let j=i ; j<arr.length ; j++) {
+			if (arr[i] >= arr[j] ) {
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+
+	return arr;
 }
 
 function selectionSort(arr) {
--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -19,20 +19,20 @@ describe('Bubble sort', () => {
 });
 
 describe('Selection sort', () => {
-  test('sorts an array', () => {
+  test.skip('sorts an array', () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
   });
 });
 
 describe('Merge sort', () => {
-  test('merge function can join together two sorted arrays', () => {
+  test.skip('merge function can join together two sorted arrays', () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
     expect(merge(left, right)).toEqual([1,2,8,10,12]);
   });
   
-  test('sorts an array', () => {
+  test.skip('sorts an array', () => {
     expect(mergeSort(getArray())).toEqual(getSortedArray());
   });
 });
