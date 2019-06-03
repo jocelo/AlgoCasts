7a7770762b91248b04f3900108d22747e0fd2c92
jocelo <gopher@gmail.com>
2019-06-03 07:02:43 -0400

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -18,15 +18,47 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+	for (let i=0 ; i<arr.length ; i++) {
+		let indexOfMin = i;
 
+		for (let j=i ; j<arr.length ; j++) {
+			if (arr[j] <= arr[indexOfMin]) {
+				indexOfMin = j;
+			}
+		}
+
+		if (indexOfMin !== i) {
+			const tmp = arr[i];
+			arr[i] = arr[indexOfMin];
+			arr[indexOfMin] = tmp;
+		}
+	}
+
+	return arr;
 }
 
 function mergeSort(arr) {
+	if (arr.length === 1) {
+		return arr;
+	}
+	const center = Math.floor(arr.length / 2);
+	const arrLeft = arr.slice(0, center);
+	const arrRight = arr.slice(center, arr.length);
 
+	return merge(mergeSort(arrLeft), mergeSort(arrRight));
 }
 
 function merge(left, right) {
+	const results = [];
+	while (left.length > 0 && right.length > 0) {
+		if (left[0] < right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
 
+	return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -19,20 +19,20 @@ describe('Bubble sort', () => {
 });
 
 describe('Selection sort', () => {
-  test.skip('sorts an array', () => {
+  test('sorts an array', () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
   });
 });
 
 describe('Merge sort', () => {
-  test.skip('merge function can join together two sorted arrays', () => {
+  test('merge function can join together two sorted arrays', () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
     expect(merge(left, right)).toEqual([1,2,8,10,12]);
   });
   
-  test.skip('sorts an array', () => {
+  test('sorts an array', () => {
     expect(mergeSort(getArray())).toEqual(getSortedArray());
   });
 });
